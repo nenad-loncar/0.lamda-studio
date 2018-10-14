0a3e54bf898feabfc2bdec2459b31acac42c6bdd
nenadloncar <nenadloncar22@gmail.com>
2018-10-14 23:52:41 +0200

    home page content code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
 window.onload = function () {
-    /*Mobile Menu*/
 
+    /* mobile menu */
     let toggleMenu = document.getElementsByClassName('toggle-menu');
-    toggleMenu[0].addEventListener('click',changeState);
+    toggleMenu[0].addEventListener('click', changeState);
 
     function changeState() {
         let menuItems = document.getElementsByClassName('menu-items-container');
         menuItems[0].classList.toggle('active-nav');
     }
 
+    /* sticky nav */
+    window.addEventListener('scroll',function() {
+        let header = document.querySelector('[data-nav="container"]'); 
+
+        if(this.scrollY >= 60) {
+            header.classList.add('fixed-header');
+        } else {
+            header.classList.remove('fixed-header');
+        }
+    });
 
 
 
