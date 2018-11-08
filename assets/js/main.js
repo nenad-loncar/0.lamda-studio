e14780ee4ced91755287c3f5b6a69c737ebda671
nenadloncar <nenadloncar22@gmail.com>
2018-11-08 23:16:12 +0100

    validate contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,131 +1,166 @@
 window.onload = function () {
 
-        /* mobile menu */
-        let toggleMenu = document.getElementsByClassName('toggle-menu');
-        toggleMenu[0].addEventListener('click', changeState);
+    /* mobile menu */
+    let toggleMenu = document.getElementsByClassName('toggle-menu');
+    toggleMenu[0].addEventListener('click', changeState);
 
-        function changeState() {
-            let menuItems = document.getElementsByClassName('menu-items-container');
-            menuItems[0].classList.toggle('active-nav');
+    function changeState() {
+        let menuItems = document.getElementsByClassName('menu-items-container');
+        menuItems[0].classList.toggle('active-nav');
+    }
+
+    /* sticky nav */
+    window.addEventListener('scroll', function () {
+        let header = document.querySelector('[data-nav="container"]');
+
+        if (this.scrollY >= 60) {
+            header.classList.add('fixed-header');
+        } else {
+            header.classList.remove('fixed-header');
         }
+    });
 
-        /* sticky nav */
-        window.addEventListener('scroll', function () {
-            let header = document.querySelector('[data-nav="container"]');
+    /* team cards slide to left or right side */
+    let cards = document.querySelectorAll('.card-container'), // select all cards
+        cardsArray = Array.from(cards), // create cards array
+        numerator = 1,
+        arrowLeft = document.querySelector('.left'),
+        arrowRight = document.querySelector('.right');
 
-            if (this.scrollY >= 60) {
-                header.classList.add('fixed-header');
-            } else {
-                header.classList.remove('fixed-header');
+
+    arrowRight.addEventListener('click', slideRight);
+    arrowLeft.addEventListener('click', slideLeft);
+
+
+    function slideRight() {
+
+        // set position for cards
+        cardsArray.forEach((e) => {
+            e.setAttribute('data-pos', numerator++);
+            if (numerator >= cards.length) {
+                numerator = 0;
             }
         });
 
-        /* team cards slide to left or right side */
-        let cards = document.querySelectorAll('.card-container'), // select all cards
-            cardsArray = Array.from(cards), // create cards array
-            numerator = 1,
-            arrowLeft = document.querySelector('.left'),
-            arrowRight = document.querySelector('.right');
-
-
-        arrowRight.addEventListener('click', slideRight);
-        arrowLeft.addEventListener('click', slideLeft);
-
-
-        function slideRight() {
-
-            // set position for cards
-            cardsArray.forEach((e) => {
-                e.setAttribute('data-pos', numerator++);
-                if (numerator >= cards.length) {
-                    numerator = 0;
-                }
-            });
-
-            // shift cards for one position to right
-            let lastElement = cardsArray[cardsArray.length - 1];
-            for (let i = cardsArray.length - 2; i >= 0; i--) {
-                cardsArray[i + 1] = cardsArray[i];
-            }
-            cardsArray[0] = lastElement;
-
+        // shift cards for one position to right
+        let lastElement = cardsArray[cardsArray.length - 1];
+        for (let i = cardsArray.length - 2; i >= 0; i--) {
+            cardsArray[i + 1] = cardsArray[i];
         }
+        cardsArray[0] = lastElement;
 
-        function slideLeft() {
+    }
 
-            // set position for cards
-            cardsArray.forEach((e) => {
-                e.setAttribute('data-pos', numerator++);
-                if (numerator >= cards.length) {
-                    numerator = 0;
-                }
-            });
+    function slideLeft() {
 
-            // shift cards for one position to left
-            let firstElement = cardsArray[0];
-            for (let i = 1; i <= cardsArray.length - 1; i++) {
-                cardsArray[i - 1] = cardsArray[i]
+        // set position for cards
+        cardsArray.forEach((e) => {
+            e.setAttribute('data-pos', numerator++);
+            if (numerator >= cards.length) {
+                numerator = 0;
             }
-            cardsArray[cardsArray.length - 1] = firstElement;
+        });
 
+        // shift cards for one position to left
+        let firstElement = cardsArray[0];
+        for (let i = 1; i <= cardsArray.length - 1; i++) {
+            cardsArray[i - 1] = cardsArray[i]
         }
+        cardsArray[cardsArray.length - 1] = firstElement;
 
+    }
 
+    /* testmonial slider */
+    let testmonialCards = document.querySelectorAll('.testmonials-card-container > div'),
+        dots = document.querySelectorAll('.indicators > span'),
+        testmonialCardArray = Array.from(testmonialCards),
+        dotsArray = Array.from(dots),
+        testNumerator = 1;
 
+    function slideTestmonial() {
+        if (testNumerator == 1) {
 
-        /* testmonial slider */
-        let testmonialCards = document.querySelectorAll('.testmonials-card-container > div'),
-            dots = document.querySelectorAll('.indicators > span'),
-            testmonialCardArray = Array.from(testmonialCards),
-            dotsArray = Array.from(dots),
+            testmonialCardArray[0].style.transform = "translateX(-170%)";
+            testmonialCardArray[0].style.zIndex = "0";
+            testmonialCardArray[1].style.transform = "translateX(-50%)";
+            testmonialCardArray[1].style.zIndex = "2";
+            testmonialCardArray[2].style.transform = "translateX(70%)";
+            testmonialCardArray[2].style.zIndex = "1";
+
+            dots[1].classList.add('active');
+            dots[0].classList.remove('active');
+            testNumerator++;
+        } else if (testNumerator == 2) {
+
+            testmonialCardArray[2].style.transform = "translateX(-170%)";
+            testmonialCardArray[2].style.zIndex = "0";
+            testmonialCardArray[0].style.transform = "translateX(-50%)";
+            testmonialCardArray[0].style.zIndex = "2";
+            testmonialCardArray[1].style.transform = "translateX(70%)";
+            testmonialCardArray[1].style.zIndex = "1";
+
+            dots[2].classList.add('active');
+            dots[1].classList.remove('active');
+            testNumerator++;
+        } else if (testNumerator == 3) {
+
+            testmonialCardArray[1].style.transform = "translateX(-170%)";
+            testmonialCardArray[1].style.zIndex = "0";
+            testmonialCardArray[2].style.transform = "translateX(-50%)";
+            testmonialCardArray[2].style.zIndex = "2";
+            testmonialCardArray[0].style.transform = "translateX(70%)";
+            testmonialCardArray[0].style.zIndex = "1";
+            testNumerator++;
+            dots[0].classList.add('active');
+            dots[2].classList.remove('active');
+
+        } else {
             testNumerator = 1;
-
-        function slideTestmonial() {
-            if (testNumerator == 1) {
-
-                testmonialCardArray[0].style.transform = "translateX(-170%)";
-                testmonialCardArray[0].style.zIndex = "0";
-                testmonialCardArray[1].style.transform = "translateX(-50%)";
-                testmonialCardArray[1].style.zIndex = "2";
-                testmonialCardArray[2].style.transform = "translateX(70%)";
-                testmonialCardArray[2].style.zIndex = "1";
-
-                dots[1].classList.add('active');
-                dots[0].classList.remove('active');
-                testNumerator++;
-            } else if (testNumerator == 2) {
-
-                testmonialCardArray[2].style.transform = "translateX(-170%)";
-                testmonialCardArray[2].style.zIndex = "0";
-                testmonialCardArray[0].style.transform = "translateX(-50%)";
-                testmonialCardArray[0].style.zIndex = "2";
-                testmonialCardArray[1].style.transform = "translateX(70%)";
-                testmonialCardArray[1].style.zIndex = "1";
-
-                dots[2].classList.add('active');
-                dots[1].classList.remove('active');
-                testNumerator++;
-            } else if (testNumerator == 3) {
-
-                testmonialCardArray[1].style.transform = "translateX(-170%)";
-                testmonialCardArray[1].style.zIndex = "0";
-                testmonialCardArray[2].style.transform = "translateX(-50%)";
-                testmonialCardArray[2].style.zIndex = "2";
-                testmonialCardArray[0].style.transform = "translateX(70%)";
-                testmonialCardArray[0].style.zIndex = "1";
-                testNumerator++;
-                dots[0].classList.add('active');
-                dots[2].classList.remove('active');
-
-            } else {
-                testNumerator = 1;
-            }
         }
+    }
 
-        setInterval(slideTestmonial, 4000);
+    setInterval(slideTestmonial, 4000);
 
 
+    /* valdiation form */
+    let formBtn = document.querySelectorAll('input'),
+        elments = document.querySelectorAll('[data-element]');
 
+
+    formBtn.forEach((e) => {
+        e.addEventListener("click", function () {
+            switch (e.id) {
+                case "submit":
+                    validateForm();
+                    break;
+                case "reset":
+                    formReset();
+                    break;
+            }
+        })
+    })
+
+
+    function validateForm() {
+        let emptyFilds = [];
+        elments.forEach((e) => {
+            if (e.value == "") {
+                emptyFilds.push(e);
+            }
+        })
+
+        if (emptyFilds.length > 0 && emptyFilds.length <= 3) {
+            alert("PLEASE FILL ALL FIELDS");
+        } else {
+            alert("MESSAGE SENT");
+        }
+    }
+
+    function formReset() {
+        elments.forEach((e) => {
+            e.value = "";
+        })
+    }
 
 
 
